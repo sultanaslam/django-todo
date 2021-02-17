0b51e09a549ac3dbb122da6992788482f21a42ff
Sultan Aslam <sultanaslam93@gmail.com>
2021-02-17 18:29:17 +0500

    list items in templates, styles, static css load

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from .models import List
+
 def home(request):
-    return render(request, 'home.html',{})
+    items = List.objects.all()
+    return render(request, 'home.html',{'items': items})
