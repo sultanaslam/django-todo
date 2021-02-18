c05aee42ad5436eb62e97ebf0497101f6c2f21c1
Sultan Aslam <sultanaslam93@gmail.com>
2021-02-18 14:36:25 +0500

    remove item and mark done item completed

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('delete/<item_id>', views.delete, name='delete'),
+    path('done/<item_id>', views.mark_done, name='mark_done'),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,9 +1,9 @@
-from django.http import request
-from django.shortcuts import render
 from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
 
-from .models import List
 from .forms import ListForm
+from .models import List
+
 def home(request):
     if request.method == 'POST':
         form = ListForm(request.POST or None)
@@ -19,3 +19,16 @@ def home(request):
 def home_response(request):
     items = List.objects.all()
     return render(request, 'home.html',{'items': items})
+
+def delete(req, item_id):
+    item = get_object_or_404(List, pk=item_id)
+    item.delete()
+    messages.success(req, ('Item Deleted'))
+    return redirect('home')
+
+def mark_done(req, item_id):
+    item = get_object_or_404(List, pk=item_id)
+    item.is_completed = True
+    item.save()
+    messages.success(req, ('Item Updated'))
+    return redirect('home')
