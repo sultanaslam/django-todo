61e25a5f95aa04367a985fe2fdd2c19b74c88387
Sultan Aslam <sultanaslam93@gmail.com>
2021-02-18 09:16:41 +0500

    add item form

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,21 @@
+from django.http import request
 from django.shortcuts import render
+from django.contrib import messages
 
 from .models import List
-
+from .forms import ListForm
 def home(request):
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Item Added'))
+            return home_response(request)
+        else:
+            return home_response(request)
+    else:
+        return home_response(request)
+
+def home_response(request):
     items = List.objects.all()
     return render(request, 'home.html',{'items': items})
